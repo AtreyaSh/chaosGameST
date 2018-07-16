2058cc069f9b4a94ee24284d836cf2b17fcd2525
Atreya Shankar <shankar@pik-potsdam.de>
2018-07-16 17:14:04 +0200

    new changes

--- src.py
+++ src.py
@@ -4,6 +4,6 @@ from gifMkr import gifMkr
 # Utilizing our images to create our gif
 
 #chaosGameBrkr(n = 100, breaks=1)
-#chaosGameBrkr(n = 10000, breaks = 500)
+chaosGameBrkr(n = 10000, breaks = 100)
 
 gifMkr(giftitle="chaosGame10000.gif")
